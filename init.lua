5cf32c2ccd8feabff5bd78ec92c8ea0129cb73ad
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2024-11-30 13:58:38 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-local n = exports["module.screen"]
-screen = setmetatable({}, {
+local _export = exports["module.screen"]
+module_screen = setmetatable({}, {
     __index = function(self, index)
         self[index] = function(...)
-            return n[index](...)
+            return _export[index](...)
         end
         return self[index]
     end
